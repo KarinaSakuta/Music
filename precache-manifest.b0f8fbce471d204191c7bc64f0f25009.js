a31ce6619134f60307ca6b3598319cb3b0add64b
Karina <47184215+KarinaSakuta@users.noreply.github.com>
2020-02-12 23:18:07 +0300

    Updates

---
+++ precache-manifest.b0f8fbce471d204191c7bc64f0f25009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7487bc7e8c0f8acbf11f285b583d5e",
+    "url": "/Music/index.html"
+  },
+  {
+    "revision": "eba0ec22dbbb03610d9c",
+    "url": "/Music/static/css/main.420d4afd.chunk.css"
+  },
+  {
+    "revision": "6343dba31acaceede970",
+    "url": "/Music/static/js/2.b1a9f966.chunk.js"
+  },
+  {
+    "revision": "eba0ec22dbbb03610d9c",
+    "url": "/Music/static/js/main.e5315ecc.chunk.js"
+  },
+  {
+    "revision": "d7f83afbc4d938a0e055",
+    "url": "/Music/static/js/runtime-main.7ec55097.js"
+  }
+]);
